e2b0d1a0139830c39f1988a96e68705d9ca29440
GaboHC10 <jherrera56@alumnos.uaq.mx>
2025-05-22 17:52:39 -0600

    Avance

--- Index.js
+++ Index.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.get('/', (req, res, next) => {
     res.status(200);
-    res.send('Bienvenidos');
+    res.send('Bienvenidos al Servidor');
 });
 
+app.get('/:nombre', (req, res, next) => {
+    req.params.name;
+    res.status(200).send('Estas en la página Nombre');
+});
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log('Server is Running...');
 });
